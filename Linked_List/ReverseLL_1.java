bd9f2f1222e98babf4f38480601512880b897f99
Pratyush Mishra <pratyush201998@gmail.com>
2021-05-20 17:20:09 +0530

    New Updates

--- Linked_List/ReverseLL_1.java
+++ Linked_List/ReverseLL_1.java
@@ -1,6 +1,286 @@
 import java.util.*;
+import java.util.LinkedList;
 
-class ReverseLL_1 
+//Recursive code
+class ReverseLL_1
+{
+    Node head;
+    Node rleft;
+
+    static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    void printList()
+    {
+        if(head == null)
+        {
+            System.out.println("List is null");
+            return;
+        }
+
+        Node nextEle = head;
+
+        while(nextEle != null)
+        {
+            System.out.print(nextEle.data + " ");
+            nextEle = nextEle.next;
+        }
+
+        System.out.println();
+    }
+
+    void reverse()
+    {
+        rleft = head;
+        reverseLL(head,0,4);
+    }
+
+    void reverseLL(Node n, int floor, int size)
+    {
+        if(n == null)
+        {
+            return;
+        }
+        floor = floor+1;
+        reverseLL(n.next, floor , size);
+        if(floor > size/2)
+        {
+
+            int temp = n.data;
+            n.data = rleft.data;
+            rleft.data = temp;
+
+            rleft = rleft.next;
+        }
+    }
+
+    void insertEnd(int data)
+    {   
+        Node new_node = new Node(data);
+
+        if(head == null)
+        {
+            head = new_node;
+            return;
+        }
+
+        Node last = head;
+
+        while(last.next != null)
+        {
+            last = last.next;
+        }
+
+        last.next = new_node;
+        return;
+    }
+    
+    public static void main(String args[])
+    {
+        ReverseLL_1 rr = new ReverseLL_1();
+        rr.insertEnd(10);
+        rr.insertEnd(20);
+        rr.insertEnd(30);           
+        rr.insertEnd(40);
+
+        rr.printList();
+
+        rr.reverse();
+
+        rr.printList();
+    }
+}
+
+/*import java.util.*;
+import java.util.LinkedList;
+
+class ReverseLL_1
+{
+    Node head;
+
+    static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    void printList()
+    {
+        if(head == null)
+        {
+            System.out.println("List is null");
+            return;
+        }
+
+        Node nextEle = head;
+
+        while(nextEle != null)
+        {
+            System.out.print(nextEle.data + " ");
+            nextEle = nextEle.next;
+        }
+
+        System.out.println();
+    }
+
+    void reverseLL(Node n)
+    {
+        if(n == null)
+        {
+            return;
+        }
+
+        reverseLL(n.next);
+        System.out.print(n.data + " ");
+    }
+
+    void insertEnd(int data)
+    {   
+        Node new_node = new Node(data);
+
+        if(head == null)
+        {
+            head = new_node;
+            return;
+        }
+
+        Node last = head;
+
+        while(last.next != null)
+        {
+            last = last.next;
+        }
+
+        last.next = new_node;
+        return;
+    }
+    
+    public static void main(String args[])
+    {
+        ReverseLL_1 rr = new ReverseLL_1();
+        rr.insertEnd(10);
+        rr.insertEnd(20);
+        rr.insertEnd(30);           
+        rr.insertEnd(40);
+
+        rr.printList();
+
+        rr.reverseLL(rr.head);
+
+    
+    }
+}
+*/
+
+/* Iterative way of reversing a Linked List
+
+import java.util.*;
+import java.util.LinkedList;
+
+class ReverseLL_1
+{
+    Node head;
+
+    static class Node
+    {
+        int data;
+        Node next;
+
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    void printList()
+    {
+        if(head == null)
+        {
+            System.out.println("List is null");
+            return;
+        }
+
+        Node nextEle = head;
+
+        while(nextEle != null)
+        {
+            System.out.print(nextEle.data + " ");
+            nextEle = nextEle.next;
+        }
+
+        System.out.println();
+    }
+
+    void reverseLL()
+    {
+        Node current = head;
+        Node prev = null, next = null;
+
+        while(current != null)
+        {
+           next = current.next;
+           current.next = prev;
+           prev = current;
+           current = next;
+        }
+
+        head = prev;
+    }
+
+    void insertEnd(int data)
+    {   
+        Node new_node = new Node(data);
+
+        if(head == null)
+        {
+            head = new_node;
+            return;
+        }
+
+        Node last = head;
+
+        while(last.next != null)
+        {
+            last = last.next;
+        }
+
+        last.next = new_node;
+        return;
+    }
+    
+    public static void main(String args[])
+    {
+        ReverseLL_1 rr = new ReverseLL_1();
+        rr.insertEnd(10);
+        rr.insertEnd(20);
+        rr.insertEnd(30);           
+        rr.insertEnd(40);
+
+        rr.printList();
+
+        rr.reverseLL();
+
+        rr.printList();
+    }
+}
+*/
+/*class ReverseLL_1 
 { 
     Node head; 
   
@@ -56,7 +336,7 @@ class ReverseLL_1
     } 
 } 
 
-
+*/
 
 
 
---
+++ Stack/TwoStacks.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class TwoStacks
+{
+    public static void main(String args[])
+    {
+        
+    }
+}
